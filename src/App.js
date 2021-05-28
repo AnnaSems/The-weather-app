5fea0e0bc9be7f6028a47bc16ec0fb723a56888c
[strodivaryann] <[sem.mm@mail.ru]>
2021-05-28 11:37:58 +1000

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+const api = {
+  API_KEY: '850179ed2538043399f4a0483428bc3a',
+  BASE_URL: 'https://api.openweathermap.org/data/2.5/'
+}
+
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      // fetch(`${api.BASE_URL}weather?q=${query}&units=metric&appid=${api.API_KEY}`, {
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   }
+      // })
+      //   .then(response => {
+      //     response.json()
+      //   })
+      //   .then(result => {
+      //     setQuery('');
+      //     return setWeather(result);
+      // })
+      const response = await fetch(`${api.BASE_URL}weather?q=${query}&units=metric&appid=${api.API_KEY}`);
+      if (response.ok) {
+        const jsonResponse = await response.json()
+        setWeather(jsonResponse);
+        setQuery('');
+      }
+    }
+  }
+
+  const getCurrentDate = () => {
+    let daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let daysOfMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const date = new Date();
+
+    let currentWeek = date.getDay() ? daysOfWeek[date.getDay() - 1] : '';
+    const currentYear = date.getFullYear();
+    let currentMonth = date.getMonth() ? daysOfMonth[date.getMonth()] : '';
+    const currentDay = date.getDate();
+
+    // daysOfWeek = daysOfWeek.filter((day, index) => [index + 1] === currentWeek);
+    // daysOfMonth = daysOfMonth.filter((month, index) => [index + 1] === currentMonth);
+    return `${currentWeek} ${currentDay} ${currentMonth} ${currentYear}`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={
+      (typeof weather.main !== 'undefined') ? ((weather.main.temp > 16) ? 'app' : 'app cold')
+        : 'app cold'}>
+      <main>
+        <div className="search-box">
+          <input type="text"
+            className="search-bar"
+            placeholder="Search city..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location">
+              <div className="get-city">{weather.name}, {weather.sys.country} </div>
+              <div className="date">{getCurrentDate()}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.floor(weather.main.temp)} ℃</div>
+              <div className="weather">{weather.weather[0].description}</div>
+            </div>
+          </div>
+        ) : ('')}
+      </main>
+    </div >
   );
 }
 
